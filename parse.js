2e85e9b4ebab60b669481058af264dada436ed1f
Joel Reis <joelhreis@gmail.com>
2018-12-27 16:55:45 -0500

    Added details function comments, no code changes.

--- parse.js
+++ parse.js
@@ -5,15 +5,15 @@
  * Potential improvements:
  * 
  * - 
- * solve predefined wildcards
+ * Solve predefined wildcards
  * `http://url.com/(one|two) -> https://url.com/$1`
  * This can be split into two rules, and handled since the results are known
  * (file exp: 4gamer.net.xml)
  * 
  * -
- * URL wildcard prefixes
- * `*.32red.com  -> /^http:\/\/(?:www\.)?32red\.com\//`
- * 
+ * Handling exclusions
+ * https://www.eff.org/https-everywhere/rulesets#exclusions
+ * `includesExclusion`
  * 
  */
 
@@ -21,11 +21,149 @@ const fs = require('fs')
 const url = require('url')
 const directory = './rules'
 
+/**
+ * 
+ * Returns all files names in a specific directory.
+ * 
+ * @param {String} directory 
+ */
 async function getFileNames(directory) {
   return new Promise((resolve, reject) => {
     fs.readdir(directory, (err, filenames) => {
       if (err) { reject(err) }
       resolve(filenames.filter(name => {return name.endsWith('.xml')}))
+    })
+  })
+}
+
+/**
+ * 
+ * Returns all URL `hosts` to include in the iOS content rules `if-domain` list.
+ * 
+ * @param {Ruleset} xmlData 
+ */
+async function getHosts(xmlData) {
+  let ruleset = xmlData.ruleset
+
+  if (defaultOff(ruleset) || includesExclusion(ruleset)) {
+    return []
+  }
+
+  let hosts = []
+  hosts = hosts.concat(ruleUpgrades(ruleset))
+  hosts = hosts.concat(targetUpgrades(ruleset))
+  return hosts
+}
+
+/**
+ * 
+ * This finds all rules that specify custom upgrade behavior (completely ignoring the `target` properties):
+ * e.g. `http://one.url.com -> https://two.url.com`
+ * Although many are not actionable with iOS HTTPSE upgrades, a good number of them are (~halfish)
+ * 
+ * Partly Doable:
+ * `^http://(?:www\.)?url\.com/ -> https://www.url.com/`
+ * 
+ *    ignores:
+ * `http://url.com -> https://www.url.com` is _not_ possible, but handling the same URL format is.
+ *    uses:
+ * `http://www.url.com -> https://www.url.com`
+ * 
+ * @param {Ruleset} ruleset An XML top level `ruleset` element
+ */
+function ruleUpgrades(ruleset) {
+  let hosts = []
+  ruleset.rule.forEach(ruleEntry => {
+    let from = RegExp(ruleEntry.$.from)
+
+    try {
+      let ruleURL =  new URL(ruleEntry.$.to)
+
+      // "downgrade" to make it hit the upgrade regex, this is not used in the actual ruleset, just to find what is needed
+      ruleURL.protocol = 'http'
+      let href = ruleURL.href
+
+      if (RegExp(from).test(href) && !includesWildcards(href)) {
+        // Just use the raw `host` for upgrade list
+        hosts.push(ruleURL.host)
+      }
+
+    } catch(error) { /* Lots of URL parsing errors, this is okay */ }
+  })
+  return hosts
+}
+
+/**
+ * 
+ * Finds all usable `target` upgrades, matching against known `rule` properties to verify they are truly usable
+ * on the iOS content format.
+ * 
+ * @param {Ruleset} ruleset An XML top level `ruleset` element
+ */
+function targetUpgrades(ruleset) {
+  let rules = ruleset.rule.map(r => RegExp(r.$.from))
+
+  // Update to `reduce`
+  return ruleset.target.map(t => {
+    let host = t.$.host
+    let target = new URL(`http://${host}`)
+    let allRuleHits = rules.filter(rule => rule.test(target))
+
+    // This verifies that each target hits only one rule, and that it is a simple rule.
+    // One rule hit is important for situations like:
+    // `www.url.com`, where rule 1 is a simple http upgrade, and the other is a subdomain rewrite:
+    // [ `http` -> `https` && `http://www.url.com` -> `https://url.com` ]
+    // Since only the first can be represented with iOS rules, this entire target must be ignored.
+    if (allRuleHits.length == 1 && allRuleHits[0] == '/^http:/') {
+      return host
+    }
+
+    return null
+  }).filter(t => t !== null)
+}
+
+/**
+ * Wildcards are part of HTTPSE spec to use URL replacement parameters.
+ * e.g.
+ * `^http://([^/:@]+)?\.fema\.gov/ -> https://$1.fema.gov/`
+ * 
+ * Here the paren rules on LHS are places inside the `$1` on the RHS
+ * Multiple params can be used, but start at index `1`, so just checking for this should be thorough enough
+ * 
+ * @param {String} rule Generally an HTTPS Upgrade `rule`'s `to` attribute 
+ */
+function includesWildcards(rule) {
+  return rule.includes("$1")
+}
+
+/**
+ * 
+ * Returns whether a specific `ruleset` is `off` by default.
+ * 
+ * @param {Ruleset} ruleset An XML top level `ruleset` element
+ */
+function defaultOff(ruleset) {
+  return ruleset.$.default_off != undefined
+}
+
+/**
+ * 
+ * Returns whether a specific `ruleset` contains exclusions.
+ * Exclusions are currently not handled, so any `ruleset` that has _any_ exceptions is currently ignored.
+ * 
+ * @param {Ruleset} ruleset An XML top level `ruleset` element
+ */
+function includesExclusion(ruleset) {
+  return ruleset.exclusion != undefined
+}
+
+async function parseXMLfile(rawFileData) {
+  return new Promise((resolve, reject) => {
+    // Required with certain parsing errors
+    // var rawFileData = rawFileData.toString().replace("\ufeff", "");
+    require('xml2js').parseString(rawFileData, function (parseError, fileContents) {
+      if (parseError) { reject(parseError) }
+      resolve(fileContents)
     })
   })
 }
@@ -44,89 +182,6 @@ function write(filePath, data) {
     if (err) { reject(err) }
     console.log("File written to: " + filePath)
   })
-}
-
-async function parseXMLfile(rawFileData) {
-  return new Promise((resolve, reject) => {
-    // Required with certain parsing errors
-    // var rawFileData = rawFileData.toString().replace("\ufeff", "");
-    require('xml2js').parseString(rawFileData, function (parseError, fileContents) {
-      if (parseError) { reject(parseError) }
-      resolve(fileContents)
-    })
-  })
-}
-
-async function getHosts(xmlData) {
-  let ruleset = xmlData.ruleset
-
-  if (defaultOff(ruleset) || includesExclusion(ruleset)) {
-    return []
-  }
-
-  let hosts = []
-
-  let rulesFrom = []
-  ruleset.rule.forEach(ruleEntry => {
-    let from = RegExp(ruleEntry.$.from)
-    rulesFrom.push(from)
-
-    try {
-      let ruleURL =  new URL(ruleEntry.$.to)
-
-      // "downgrade" to make it hit the upgrade regex, this is not used in the actual ruleset, just to find what is needed
-      ruleURL.protocol = 'http'
-      let href = ruleURL.href
-
-      if (RegExp(from).test(href) && !includesWildcards(href)) {
-        // Just use the raw `host` for upgrade list
-        hosts.push(ruleURL.host)
-      }
-
-    } catch(error) { /* Lots of URL parsing errors, this is okay */ }
-  })
-
-  return hosts.concat(simpleUpgrades(ruleset))
-}
-
-/**
- * Wildcards are part of HTTPSE spec to use URL replacement parameters
- * e.g.
- * ^http://([^/:@]+)?\.fema\.gov/ -> https://$1.fema.gov/
- * 
- * Here the paren rules on LHS are places inside the `$1` on the RHS
- * Multiple params can be used, but start at index `1`, so just checking for this should be thorough enough
- * 
- * @param {String} rule Generally an HTTPS Upgrade `rule`'s `to` attribute 
- */
-function includesWildcards(rule) {
-  return rule.includes("$1")
-}
-
-function simpleUpgrades(ruleset) {
-  let rules = ruleset.rule.map(r => RegExp(r.$.from))
-
-  // Update to `reduce`
-  return ruleset.target.map(t => {
-    let host = t.$.host
-    let target = new URL(`http://${host}`)
-    let allRuleHits = rules.filter(rule => rule.test(target))
-
-    // This verifies that each target hits only one rule, and that it is a simple
-    if (allRuleHits.length == 1 && allRuleHits[0] == '/^http:/') {
-      return host
-    }
-
-    return null
-  }).filter(t => t !== null)
-}
-
-function defaultOff(ruleset) {
-  return ruleset.$.default_off != undefined
-}
-
-function includesExclusion(ruleset) {
-  return ruleset.exclusion != undefined
 }
 
 async function run() {
